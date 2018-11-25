ab198abebb2c72a56c487f66a160ffbb70d78272
t4skforce <t4skforce1337@gmail.com>
2018-11-25 17:01:52 +0100

    Update jenkins_cert.sh

--- jenkins_cert.sh
+++ jenkins_cert.sh
@@ -7,7 +7,7 @@ if [ ! -f "$CERT_FOLDER/jenkins.pem" ] || [ ! -f "$CERT_FOLDER/jenkins.key" ]; t
         && openssl req -new -key "$CERT_FOLDER/jenkins.key" -out "$CERT_FOLDER/jenkins.csr" -subj "/C=US/ST=Example/L=Example/O=Example Company Inc./CN=www.example.com" \
         && openssl x509 -req -days 3560 -in "$CERT_FOLDER/jenkins.csr" -signkey "$CERT_FOLDER/jenkins.key" -out "$CERT_FOLDER/jenkins.pem" \
         && rm "$CERT_FOLDER/jenkins.csr" \
-	&& chown -R jenkins:jenkins "$CERT_FOLDER"
+        && chown -R jenkins:jenkins "$CERT_FOLDER"
 fi
 # install custom root ca if configured
 if [ ! -z "$ROOT_CA" ]; then
